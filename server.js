37cec7a53a2f7d66f2255d8af6e2d24806602056
joxpulp <joxpulp@gmai.com>
2021-07-20 22:37:27 -0500

    arrow function added: random values every time the page is reloaded

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const http = require("http");
+const http = require('http');
 
-const product = {
-	id: Math.floor(Math.random() * (11 - 1) + 1),
-	title: `Producto ${Math.floor(Math.random() * (11 - 1) + 1)}`,
-	price: Math.round((Math.random() * (1000 - 0) + 0) * 100) / 100,
-	thumbnail: `Foto ${Math.floor(Math.random() * (11 - 1) + 1)}`,
+const product = () => {
+	return {
+		id: Math.floor(Math.random() * (11 - 1) + 1),
+		title: `Producto ${Math.floor(Math.random() * (11 - 1) + 1)}`,
+		price: Math.round((Math.random() * (1000 - 0) + 0) * 100) / 100,
+		thumbnail: `Foto ${Math.floor(Math.random() * (11 - 1) + 1)}`,
+	};
 };
 
 const server = http.createServer((request, response) => {
-	response.end(JSON.stringify(product, null, 2));
+	response.end(JSON.stringify(product(), null, 2));
 });
 
 server.listen(3000, () => {
-	console.log("Servidor escuchando en el puerto 3000");
+	console.log('Servidor escuchando en el puerto 3000');
 });
